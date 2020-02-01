2f39051ec186dfc130d199548422dbaf9484e646
Austin Oldham <alo52@pitt.edu>
2020-01-31 23:14:40 -0500

    Added more templates

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index.html')
 def root():
-  return render_template('index.html', page_title='Home')
+	return render_template('index.html', page_title='Home')
+
+@app.route('/about')
+def about():
+	return render_template('about.html', page_title='About')
+
+@app.route('/portfolio')
+def portfolio():
+	return render_template('portfolio.html', page_title='Portfolio')
+
+@app.route('/contact.html')
+def contact():
+	return render_template('contact.html', page_title='Contact')
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8080)
+		app.run(host='127.0.0.1', port=8080)
